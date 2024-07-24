ef388b76144eab605fe68f4cfd3a5e240dd4951b
xxube <54475474+xxube@users.noreply.github.com>
2024-07-24 16:10:31 +0800

    Update GNN.py

--- Model/GNN.py
+++ Model/GNN.py
@@ -4,34 +4,34 @@ import torch.nn.functional as F
 import dgl
 from MPNN_layer import MPNN
 
-class MODEL(nn.Module):
-    def __init__(self, _setup_dict):
-        super(MODEL, self).__init__()
+class CustomModel(nn.Module):
+    def __init__(self, config):
+        super(CustomModel, self).__init__()
 
-        self.MPNN1 = MPNN(_setup_dict['MPNN'])
+        self.gnn_layer = MPNN(config['MPNN'])
 
-        self.dense_dict = nn.ModuleDict()
+        self.dense_layers = nn.ModuleDict()
 
-        for _block in ['dense_E1', 'dense_E2', 'dense_output']:
-            _block_dict = _setup_dict[_block]
+        for layer_name in ['dense_E1', 'dense_E2', 'dense_output']:
+            layer_config = config[layer_name]
 
-            units = _block_dict['units']
-            activation = _block_dict['activation']
-            use_bias = _block_dict['use_bias']
+            units = layer_config['units']
+            activation = layer_config['activation']
+            use_bias = layer_config['use_bias']
 
-            kernel_regularizer = _block_dict['kernel_regularizer']
+            kernel_regularizer = layer_config['kernel_regularizer']
             if kernel_regularizer == 'None':
                 kernel_regularizer = None
             elif isinstance(kernel_regularizer, dict):
-                for k, v in kernel_regularizer.items():
-                    _type = k
-                    _value = v
-                if _type == "l1":
+                for key, value in kernel_regularizer.items():
+                    reg_type = key
+                    reg_value = value
+                if reg_type == "l1":
                     kernel_regularizer = nn.L1Loss()
-                elif _type == "l2":
+                elif reg_type == "l2":
                     kernel_regularizer = nn.MSELoss()
 
-            kernel_constraint = _block_dict['kernel_constraint']
+            kernel_constraint = layer_config['kernel_constraint']
             if kernel_constraint == 'NonNeg':
                 kernel_constraint = lambda x: torch.clamp(x, min=0.0)
             elif kernel_constraint == 'None':
@@ -44,79 +44,79 @@ class MODEL(nn.Module):
             else:
                 activation_fn = getattr(F, activation)
 
-            self.dense_dict[_block] = nn.Sequential(
+            self.dense_layers[layer_name] = nn.Sequential(
                 dense_layer,
                 activation_fn
             )
         
-        self._h = 6.62607015 * (10 ** -34)
-        self._kb = 1.380649 * (10 ** -23)
-        self._J_to_kcal = 4184
-        self._R = 8.31
-        self._T = 273 + 80
-        self._1000s_to_h = 3.6
+        self.h_constant = 6.62607015 * (10 ** -34)
+        self.kb_constant = 1.380649 * (10 ** -23)
+        self.j_to_kcal = 4184
+        self.R_constant = 8.31
+        self.temperature = 273 + 80
+        self.s_to_h = 3.6
 
-        self.useArrheniusEq = True
-        self.useVertexValue = False
-        self.forAnalyze = False
+        self.use_arrhenius_eq = True
+        self.use_vertex_value = False
+        self.for_analysis = False
 
-    def ArrheniusEq(self, Ea):
-        return self._kb * self._T / self._h / self._1000s_to_h * torch.exp(-Ea * self._J_to_kcal / self._R / self._T)
+    def compute_arrhenius(self, Ea):
+        return self.kb_constant * self.temperature / self.h_constant / self.s_to_h * torch.exp(-Ea * self.j_to_kcal / self.R_constant / self.temperature)
 
-    def stopuseArrheniusEq(self):
-        self.useArrheniusEq = False
+    def disable_arrhenius(self):
+        self.use_arrhenius_eq = False
 
-    def startuseArrheniusEq(self):
-        self.useArrheniusEq = True
+    def enable_arrhenius(self):
+        self.use_arrhenius_eq = True
 
-    def stopuseVertexValue(self):
-        self.useVertexValue = False
+    def disable_vertex_value(self):
+        self.use_vertex_value = False
 
-    def startuseVertexValue(self):
-        self.useVertexValue = True
+    def enable_vertex_value(self):
+        self.use_vertex_value = True
 
-    def forward(self, input_data, return_FCNN=False, return_LayersInt=False, _device=None, training=None):
-        _INT_dict = {}
+    def forward(self, input_data, return_full=False, return_intermediate=False, device=None, training=None):
+        intermediate_dict = {}
 
-        rct_inputs_data, lig_inputs_data = input_data
+        reactant_data, ligand_data = input_data
 
-        # treat rct
-        rct_inputs = torch.tensor(rct_inputs_data, dtype=torch.float32)
+        # process reactant data
+        reactant_inputs = torch.tensor(reactant_data, dtype=torch.float32)
 
-        # treat lig
-        if _device is not None:
-            lig_inputs = lig_inputs_data.to(_device)
+        # process ligand data
+        if device is not None:
+            ligand_inputs = ligand_data.to(device)
         else:
-            lig_inputs = lig_inputs_data
+            ligand_inputs = ligand_data
 
-        lig_inputs = self.MPNN1(lig_inputs)
-        _INT_dict['lig_inputs'] = lig_inputs
+        ligand_inputs = self.gnn_layer(lig_inputs)
+        intermediate_dict['ligand_inputs'] = lig_inputs
 
-        # (optional) return vertex value directly
-        if self.useVertexValue:
+        # optionally return vertex value directly
+        if self.use_vertex_value:
             return lig_inputs
 
-        if self.forAnalyze:
-            return rct_inputs_data, lig_inputs
+        if self.for_analysis:
+            return reactant_data, lig_inputs
 
-        # E1
-        inputs_E1 = torch.cat([rct_inputs, lig_inputs], dim=1)
-        inputs_E1 = self.dense_dict['dense_E1'](inputs_E1)
-        _INT_dict['inputs_E1'] = inputs_E1
+        # Dense layer E1
+        inputs_E1 = torch.cat([reactant_inputs, lig_inputs], dim=1)
+        inputs_E1 = self.dense_layers['dense_E1'](inputs_E1)
+        intermediate_dict['inputs_E1'] = inputs_E1
 
-        # E2
-        inputs_E2 = self.dense_dict['dense_E2'](lig_inputs)
-        _INT_dict['inputs_E2'] = inputs_E2
+        # Dense layer E2
+        inputs_E2 = self.dense_layers['dense_E2'](lig_inputs)
+        intermediate_dict['inputs_E2'] = inputs_E2
 
-        # concat E1 and E2
-        inputs_E1_E2 = torch.cat([inputs_E1, inputs_E2], dim=1)
-        outputs = self.dense_dict['dense_output'](inputs_E1_E2)
+        # concatenate E1 and E2
+        concatenated_inputs = torch.cat([inputs_E1, inputs_E2], dim=1)
+        outputs = self.dense_layers['dense_output'](concatenated_inputs)
 
-        if self.useArrheniusEq:
-            outputs = self.ArrheniusEq(outputs)
+        if self.use_arrhenius_eq:
+            outputs = self.compute_arrhenius(outputs)
 
-        if return_FCNN:
-            return outputs, lig_inputs, self.dense_dict
-        if return_LayersInt:
-            return outputs, lig_inputs, self.dense_dict, _INT_dict
+        if return_full:
+            return outputs, lig_inputs, self.dense_layers
+        if return_intermediate:
+            return outputs, lig_inputs, self.dense_layers, intermediate_dict
         return outputs
